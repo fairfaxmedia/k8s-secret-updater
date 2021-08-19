f15fb926affeaa03ea942362d72eb2b76c31a41c
matt hope <matt.hope@fairfaxmedia.com.au>
2021-08-19 11:54:33 +1000

    Version 1.2.0
    - Code refactors
    - Detect bad secret format (bad base64)
    - Authenticate to Kubernetes cluster with a token (in addition to certificate or user/pass)
    - Create Kubernetes namespace if not existing
    - Handle situation where kubernetes namespace has no deployments

--- secretupdater/secretupdater/models.py
+++ secretupdater/secretupdater/models.py
@@ -22,17 +22,18 @@ import base64
 # silence some warnings:
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 if app.config.get('AUTH_METHOD') == 'header':
     from secretupdater.headerclient import HeaderClient as ConfidantClient
+
     app.logger.debug('Using Header ConfidantClient - should not be done in prod!!')
 else:
     from confidant_client import ConfidantClient
 
 
 def process(event):
-
     affected_services = event.get('services') or []
     errors = []
     result_code = 200
@@ -58,146 +59,69 @@ def process(event):
             service_name = service_details['id']
             namespace = _service_to_namespace(service_name)
             credentials = service_details.get('credentials') or []
+
             # Find which k8s clusters the service lives on, and get their login details
             k8s_clusters = [c for c in credentials if c.get('enabled') and c.get('name').startswith('k8s-cluster-')]
             secret_collection = [s for s in credentials if s.get('enabled') and not s.get('name').startswith('k8s-cluster-')]
             app.logger.debug({"k8s_clusters": k8s_clusters, "secret_collection": secret_collection})
 
+            # Build up the secret data.
+            try:
+                secrets = _parse_secret_collection(secret_collection=secret_collection, namespace=namespace)
+            except BadSecretFormat as e:
+                errors.append("Error in Secret data: " + str(e))
+                result_code = 400
+                break
+
             # Create a hashed value, to see if the data has changed.
             hashedval = hashlib.md5(str(repr(credentials)).encode('utf-8')).hexdigest()
 
-            for kube in k8s_clusters:
-                if _get_credential(kube, 'certificate-authority-data'):  # Assuming that if we are given the k8s CA, we're using certs
-                    app.logger.debug("Found CA data, assuming we're using x509 for auth")
-                    config = {
-                        "clusters": [
-                            {
-                                "name": kube.get('id'),
-                                "cluster": {
-                                    "server": kube.get('metadata').get('endpoint'),
-                                    "certificate-authority-data": _get_credential(kube, 'certificate-authority-data')
-                                },
-                            },
-                        ],
-                        "contexts": [
-                            {
-                                "name": kube.get('id'),
-                                "context": {
-                                    "cluster": kube.get('id'),
-                                    "user": "admin"
-                                },
-                            }
-                        ],
-                        "current-context": kube.get('id'),
-                        "users": [
-                            {
-                                "name": "admin",
-                                "user": {
-                                    "username": _get_credential(kube, 'username'),
-                                    "client-certificate-data": _get_credential(kube, 'client-certificate-data'),
-                                    "client-key-data": _get_credential(kube, 'client-key-data')
-                                }
-                            }
-                        ]
-                    }
-                else:
-                    app.logger.debug("No CA data available, assuming username/password auth")
-                    config = {
-                        "clusters": [
-                            {
-                                "name": kube.get('id'),
-                                "cluster": {
-                                    "server": kube.get('metadata').get('endpoint'),
-                                    "insecure-skip-tls-verify": True
-                                },
-                            },
-                        ],
-                        "contexts": [
-                            {
-                                "name": kube.get('id'),
-                                "context": {
-                                    "cluster": kube.get('id'),
-                                    "user": "admin"
-                                },
-                            }
-                        ],
-                        "current-context": kube.get('id'),
-                        "users": [
-                            {
-                                "name": "admin",
-                                "user": {
-                                    "username": _get_credential(kube, 'username'),
-                                    "password": _get_credential(kube, 'password')
-                                }
-                            }
-                        ]
-                    }
-                app.logger.debug("config: %s", config)
+            k8s_auth = _get_k8s_auth(credentials=credentials)
+
+            for config in k8s_auth:
+                app.logger.debug({"config": config})
                 try:
                     kube_connection = pykube.HTTPClient(pykube.KubeConfig(config))
-                    for entry in secret_collection:
 
-                        if 'secret-name' in entry.get('metadata'):
-                            secret_name = entry.get('metadata').get('secret-name')
-                        else:
-                            secret_name = entry.get('name')
+                    for k8s_secret in secrets.values():
 
-                        if 'secret-type' in entry.get('metadata'):
-                            secret_type = entry.get('metadata').get('secret-type')
-                        else:
-                            secret_type = 'Opaque'
-
-                        secret_pairs = entry.get('credential_pairs')
-
-                        if 'secret-case' in entry.get('metadata'):
-                            upper_case_creds = entry.get('metadata').get('secret-case').split()
-                            for k in secret_pairs:
-                                if k in upper_case_creds:
-                                    v = secret_pairs[k]
-                                    secret_pairs.pop(k)
-                                    secret_pairs[k.upper()] = v
-
-                        if 'secret-case-regex' in entry.get('metadata'):
-                            upper_case_regex = entry.get('metadata').get('secret-case-regex').split("\n")
-                            for r in upper_case_regex:
-                                matching = list(filter(lambda x: re.search(r, x), secret_pairs))
-                                for k in matching:
-                                    secret_pairs[k.upper()] = secret_pairs[k]
-                                    secret_pairs.pop(k)
-
-                        for k, v in secret_pairs.items():
-                            if v.lower().startswith("base64:"):
-                                secret_pairs[k] = v[7:]
-                            else:
-                                secret_pairs[k] = base64.b64encode(v.encode('utf-8')).decode('ascii')
-
-                        k8s_secret = {
-                            "apiVersion": "v1",
-                            "kind": "Secret",
-                            "metadata": {
-                                "name": secret_name,
-                                "namespace": namespace
-                            },
-                            "type": secret_type,
-                            "data": secret_pairs
-                        }
-
-                        app.logger.debug(k8s_secret)
                         if pykube.Secret(kube_connection, k8s_secret).exists():
                             pykube.Secret(kube_connection, k8s_secret).delete()  # We delete, rather than update(), as updates seems to keep deleted data
+
+                        k8s_namespace = {"apiVersion": "v1", "kind": "Namespace", "metadata": {"name": namespace}}
+                        if not pykube.Namespace(kube_connection, k8s_namespace).exists():
+                            errors.append("Creating namespace %s" % (namespace))
+                            pykube.Namespace(kube_connection, k8s_namespace).create()
+
                         pykube.Secret(kube_connection, k8s_secret).create()
+
                     _trigger_deployment(kube_connection, namespace, checksum=hashedval)  # After updating secrets, we trigger a rolling update in the namespace
-                except Exception as e:
+
+                except KeyError as e:
                     errors.append(str(e))
                     result_code = 500
-                    app.logger.debug(e)
+                    app.logger.debug({"error": e})
+
+                except NamespaceNoDeploymentError as e:
+                    errors.append(str(e))
+                    result_code = 200
+
     return {'code': result_code, 'errors': errors}
 
 
 def _trigger_deployment(kube_connection, namespace, timestamp=datetime.datetime.utcnow(), checksum=None):
-    app.logger.debug(str(checksum))
-    res = pykube.Deployment.objects(kube_connection).filter(namespace=namespace).all().response
-    for deployment in res['items']:
+    res = pykube.Deployment.objects(kube_connection).filter(namespace=namespace).all()
+
+    response = {"items": []}
+    try:
+        response = res.response
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            raise NamespaceNoDeploymentError("no deployments found in namespace '%s'" % (namespace))
+
+        raise
+
+    for deployment in response['items']:
         if 'annotations' not in deployment['spec']['template']['metadata']:
             deployment['spec']['template']['metadata']['annotations'] = {}
         if 'annotations' in deployment['metadata'] and 'secretupdater.ffx.io/skip_reload' in deployment['metadata']['annotations']:
@@ -244,3 +168,208 @@ def _setup_confidant_client(service):
     )
     app.logger.debug(client.config)
     return client
+
+
+def _parse_secret_collection(secret_collection, namespace):
+    secrets = {}
+
+    for entry in secret_collection:
+        secret = _parse_secret_entry(entry, namespace)
+
+        if 'metadata' not in secret:
+            continue
+        if 'name' not in secret['metadata']:
+            continue
+
+        name = secret['metadata']['name']
+        secrets[name] = secret
+
+    return secrets
+
+
+def _parse_secret_entry(entry, namespace):
+    if 'secret-name' in entry.get('metadata'):
+        secret_name = entry.get('metadata').get('secret-name')
+    else:
+        secret_name = entry.get('name')
+
+    if 'secret-type' in entry.get('metadata'):
+        secret_type = entry.get('metadata').get('secret-type')
+    else:
+        secret_type = 'Opaque'
+
+    secret_pairs = entry.get('credential_pairs')
+
+    if 'secret-case' in entry.get('metadata'):
+        upper_case_creds = entry.get('metadata').get('secret-case').split()
+        for key in secret_pairs:
+            if key in upper_case_creds:
+                val = secret_pairs[key]
+                secret_pairs.pop(key)
+                secret_pairs[key.upper()] = val
+
+    if 'secret-case-regex' in entry.get('metadata'):
+        upper_case_regex = entry.get('metadata').get('secret-case-regex').split("\n")
+        for r in upper_case_regex:
+            matching = list(filter(lambda x: re.search(r, x), secret_pairs))
+            for k in matching:
+                secret_pairs[k.upper()] = secret_pairs[k]
+                secret_pairs.pop(k)
+
+    for key, val in secret_pairs.items():
+        if val.lower().startswith("base64:"):
+            secret_pairs[key] = val[7:]
+
+            # Check that the remainder of the string is valid base64.
+            try:
+                base64.b64decode(secret_pairs[key])
+            except Exception:
+                raise BadSecretFormat(
+                    {
+                        "key": key,
+                        "entry": {
+                            "id": entry["id"],
+                            "name": entry["name"],
+                            "revision": entry["revision"],
+                        },
+                    }
+                )
+
+        else:
+            secret_pairs[key] = base64.b64encode(val.encode('utf-8')).decode('ascii')
+
+    k8s_secret = {
+        "apiVersion": "v1",
+        "kind": "Secret",
+        "metadata": {"name": secret_name, "namespace": namespace},
+        "type": secret_type,
+        "data": secret_pairs,
+    }
+
+    return k8s_secret
+
+
+def _get_k8s_auth(credentials):
+    k8s_auth = []
+    k8s_clusters = [c for c in credentials if c.get('enabled') and c.get('name').startswith('k8s-cluster-')]
+
+    for kube in k8s_clusters:
+        id = kube.get('id')
+        if _get_credential(kube, 'certificate-authority-data'):  # Assuming that if we are given the k8s CA, we're using certs
+            app.logger.debug("%s: Found CA data, assuming we're using x509 for auth" % (id))
+            config = {
+                "clusters": [
+                    {
+                        "name": id,
+                        "cluster": {
+                            "server": kube.get('metadata').get('endpoint'),
+                            "certificate-authority-data": _get_credential(kube, 'certificate-authority-data'),
+                        },
+                    },
+                ],
+                "contexts": [
+                    {
+                        "name": id,
+                        "context": {
+                            "cluster": id,
+                            "user": "admin"
+                        },
+                    }
+                ],
+                "current-context": id,
+                "users": [
+                    {
+                        "name": "admin",
+                        "user": {
+                            "username": _get_credential(kube, 'username'),
+                            "client-certificate-data": _get_credential(kube, 'client-certificate-data'),
+                            "client-key-data": _get_credential(kube, 'client-key-data'),
+                        },
+                    }
+                ],
+            }
+        elif _get_credential(kube, 'token'):
+            app.logger.debug("%s: Kubernetes token provided" % (id))
+            config = {
+                "clusters": [
+                    {
+                        "name": id,
+                        "cluster": {
+                            "server": kube.get('metadata').get('endpoint'),
+                            "insecure-skip-tls-verify": True
+                        },
+                    },
+                ],
+                "contexts": [
+                    {
+                        "name": id,
+                        "context": {
+                            "cluster": id,
+                            "user": "admin"
+                        },
+                    }
+                ],
+                "current-context": id,
+                "users": [
+                    {
+                        "name": "admin",
+                        "user": {
+                            "token": _get_credential(kube, 'token')
+                        }
+                    }
+                ]
+            }
+        elif _get_credential(kube, 'username'):
+            app.logger.debug("%s: Kubernetes username/password provided" % (id))
+            config = {
+                "clusters": [
+                    {
+                        "name": id,
+                        "cluster": {
+                            "server": kube.get('metadata').get('endpoint'),
+                            "insecure-skip-tls-verify": True,
+                        },
+                    },
+                ],
+                "contexts": [
+                    {
+                        "name": id,
+                        "context": {
+                            "cluster": id,
+                            "user": "admin"
+                        },
+                    }
+                ],
+                "current-context": id,
+                "users": [
+                    {
+                        "name": "admin",
+                        "user": {
+                            "username": _get_credential(kube, 'username'),
+                            "password": _get_credential(kube, 'password'),
+                        },
+                    }
+                ],
+            }
+
+        k8s_auth.append(config)
+
+    return k8s_auth
+
+
+class NamespaceNoDeploymentError(Exception):
+    """A Namespace does not have any Deployment objects"""
+
+    pass
+
+
+class KuberentesError(Exception):
+    pass
+
+
+class KuberentesConnectionError(KuberentesError):
+    pass
+
+
+class BadSecretFormat(Exception):
+    pass
